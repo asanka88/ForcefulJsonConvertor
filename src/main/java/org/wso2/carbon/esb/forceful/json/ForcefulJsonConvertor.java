f8c512c75851270583740c7994b83ed8ef5da349
Your Name <you@example.com>
2014-11-12 16:51:00 -0500

    changing class description

--- src/main/java/org/wso2/carbon/esb/forceful/json/ForcefulJsonConvertor.java
+++ src/main/java/org/wso2/carbon/esb/forceful/json/ForcefulJsonConvertor.java
@@ -13,7 +13,8 @@ import com.sun.phobos.script.javascript.RhinoScriptEngineFactory;
 /***
  * Reason for this mediator: When an XML payload is returning from the Backend , users cannot retrieve the 
  * JSON payload by using mc.getPayloadJSON() in the script mediator.
- *IMPORTANT: Use this before script mediator .. 
+ *IMPORTANT: Build the jar, and copy to {carbon.home}/repository/components/lib and restart the server.
+ * Use this before script mediator . This has been tested with ESB 4.7.0 only.
  *         <class name="org.wso2.carbon.esb.forceful.json.ForcefulJsonConvertor"/>
  *         <script .....
  To solve this problem this mediator was developed
